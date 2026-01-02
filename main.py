e18f58d47d21cd9518f97757fa8a820b7d22b35f
abogadovillegasminero-blip <abogadovillegasminero@gmail.com>
2026-01-01 23:47:49 -0500

    Add login page route with Jinja2 template

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/login", response_class=HTMLResponse)
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
